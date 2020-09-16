f00623871004c3d46ce872913792f7dbde51ed18
amandacutrer <23acutrer@athenian.org>
2020-09-16 23:30:29 +0000

    binary to decimal converter

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        String bin = binary.replaceAll(" ", "");
+       int binNum = Integer.parseInt(bin);
+       int answer = 0;
+       int place = 1;
+        while (binNum > 0){
+            answer = answer + ((binNum % 10)*place);
+            binNum /= 10; 
+            place = place * 2;
+        }
+        return answer;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format: ");
+        int num = scan.nextInt();
+        String number = num + " ";
+        System.out.println(number);
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(number));
+           scan.close();
     }
 }
